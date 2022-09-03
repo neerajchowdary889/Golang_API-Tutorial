ac47f72e09004f635a3da48f66281f67700fba54
neerajchowdary889 <neerajchowdary889@gmail.com>
2022-09-03 21:16:13 +0530

    updatebooks, bookbyID

--- go_API.go
+++ go_API.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 )
 
 type book struct {
+	//These are the fields that we created and field names in json file will in all small.
 	ID       string `json:"id"`
 	Title    string `json:"title"`
 	Author   string `json:"author"`
@@ -26,18 +29,47 @@ func getbooks(c *gin.Context) {
 }
 
 func updatebooks(c *gin.Context) {
+	//We can enter book details in newbook and we make it a POST request to add to books.
 	newbook := book{ID: "4", Title: "Atomic Habits", Author: "Dont know", Quantity: 3}
 	books = append(books, newbook)
 	//If the newbook was created then we will get http : 201 that means Req was successfull and resource has been created.
 	//we need to enter " curl localhost:8080/books --request "POST" " command to get newbook details and add to the books struct.
 	c.IndentedJSON(http.StatusCreated, newbook)
-
 }
+
+func getBookbyID(id string) (*book, error) {
+	//Here getBookbyID take input int and search that id exists or not in the books array.
+	for i, bookk := range books {
+		if bookk.ID == id {
+			return &books[i], nil
+			//here if id exists then it returns the position of book. we used pointers, one of the best feature of GO.
+		}
+	}
+	return nil, errors.New("No book bud...")
+}
+
+func bookbyID(c *gin.Context) {
+	// command is "curl localhost:8080/books/2" or 1 anything.
+	// If we enter 2 we get output of book of ID 2.
+	id := c.Param("id")
+	book, err := getBookbyID(id)
+
+	if err != nil {
+		log.Panic("panicking...")
+		return
+	}
+	c.IndentedJSON(http.StatusOK, book)
+}
+
 func main() {
 	router := gin.Default()
 	//.GET is the get request we creating to get the data.
 	//.RUN is the request to get server started.
-	router.GET("/books", getbooks)
-	router.POST("/books", updatebooks)
-	router.Run("localhost:8080")
+	router.GET("/books", getbooks) // To get the all books.
+
+	router.GET("/books/:id", bookbyID) // To get the book with any ID.
+
+	router.POST("/books", updatebooks) // This is post req. UpdateBook
+
+	router.Run("localhost:8080") // for initilizing the server.
 }
